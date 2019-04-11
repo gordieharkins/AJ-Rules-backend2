fe6cdc8cd63416321671703568240f40dd6ba47c
Eman Fatima <eman.fatima@spsnet.com>
2019-04-11 10:37:09 +0500

    username bugfix

--- BLL/util/passport.js
+++ BLL/util/passport.js
@@ -11,17 +11,17 @@ var jwt = require('jsonwebtoken');
 // Setup work and export for the JWT passport strategy
 module.exports = function(passport) {
     var opts = {};
-    // //console.log("=========================PASSPORT JwtStrategy=============================================");
+    // console.log("=========================PASSPORT JwtStrategy=============================================");
 
     opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
     opts.secretOrKey = auth.secret;
-    // //console.log("opts:",opts);
+    // console.log("opts:",opts);
     passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
         var credentials = jwt_payload;
         var query = `MATCH (user:user)
                     WHERE user.email1= {email} AND id(user) = {userId}
                     OPTIONAL Match(role:UserRole) where role.name = user.role
-                    RETURN id(user) AS userId, properties(role) as roles, user.name as userName`;
+                    RETURN id(user) AS userId, properties(role) as roles, user.username as userName`;
         db.cypher({
             query: query,
             params:{
@@ -38,5 +38,5 @@ module.exports = function(passport) {
 
     }));
 
-    // //console.log("=============================================================================================");
+    // console.log("=============================================================================================");
 };
